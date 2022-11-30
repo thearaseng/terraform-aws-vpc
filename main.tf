680fac697aff9fa649edc5ba455220a6db1011ac
Theara Seng <me@thearaseng.com>
2022-11-30 23:39:55 +0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,85 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = "${var.project_name}-VPC"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count      = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.public_subnet_cidrs, count.index)
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count      = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.private_subnet_cidrs, count.index)
+
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name} IG"
+  }
+}
+
+resource "aws_eip" "nat_gateways" {
+  count = length(aws_subnet.private_subnets)
+  vpc   = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  count         = length(aws_subnet.private_subnets)
+  allocation_id = element(aws_eip.nat_gateways, count.index).id
+  subnet_id     = element(aws_subnet.private_subnets, count.index).id
+}
+
+resource "aws_route_table" "public_subnets" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Public Subnet Route Table"
+  }
+}
+
+resource "aws_route_table" "private_subnets" {
+  count  = length(aws_nat_gateway.nat_gw)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = element(aws_nat_gateway.nat_gw, count.index).id
+  }
+
+  tags = {
+    Name = "Private Subnet Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+  count          = length(aws_subnet.public_subnets)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public_subnets.id
+}
+
+resource "aws_route_table_association" "private_subnet_asso" {
+  count          = length(aws_subnet.private_subnets)
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  route_table_id = element(aws_route_table.private_subnets[*].id, count.index)
+}
