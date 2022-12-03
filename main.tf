10f6a28620e9823a73f74878bb684a71a8cc3e78
Theara Seng <me@thearaseng.com>
2022-12-03 23:44:51 +0700

    refactor

--- main.tf
+++ main.tf
@@ -6,10 +6,19 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name} IG"
+  }
+}
+
 resource "aws_subnet" "public_subnets" {
-  count      = length(var.public_subnet_cidrs)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.public_subnet_cidrs, count.index)
+  count             = length(var.azs)
+  vpc_id            = aws_vpc.main.id
+  availability_zone = element(var.azs, count.index)
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
 
   tags = {
     Name = "Public Subnet ${count.index + 1}"
@@ -17,20 +26,13 @@ resource "aws_subnet" "public_subnets" {
 }
 
 resource "aws_subnet" "private_subnets" {
-  count      = length(var.private_subnet_cidrs)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.private_subnet_cidrs, count.index)
+  count             = length(var.azs)
+  vpc_id            = aws_vpc.main.id
+  availability_zone = element(var.azs, count.index)
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
 
   tags = {
     Name = "Private Subnet ${count.index + 1}"
-  }
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.vpc_name} IG"
   }
 }
 
